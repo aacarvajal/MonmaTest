b6c36180d5ed419bc6561cec853829a00ca36e24
Adrian Carvajal <adrycarpe94@gmail.com>
2018-10-07 16:58:20 +0200

    Segundo commit (Temporizador)

--- app/src/main/java/com/example/adrian/monmatest/InicioActivity.java
+++ app/src/main/java/com/example/adrian/monmatest/InicioActivity.java
@@ -1,13 +1,29 @@
 package com.example.adrian.monmatest;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class InicioActivity extends AppCompatActivity {
 
+    private static int DURACION = 4000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(InicioActivity.this, SegundoActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },DURACION);
+
     }
 }
