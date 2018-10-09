ad2c639354a3232bd312374be9d0b64c0854e06a
Adrian Carvajal <adrycarpe94@gmail.com>
2018-10-09 14:56:05 +0200

    Tercer commit (Log y quitar actionbar de la activity principal)

---
+++ app/src/main/java/com/example/adrian/monmatest/Constantes.java
@@ -0,0 +1,9 @@
+package com.example.adrian.monmatest;
+
+public class Constantes {
+
+    public static final String USER = "root";
+    public static final String PASSWORD = "";
+
+
+}
--- app/src/main/java/com/example/adrian/monmatest/InicioActivity.java
+++ app/src/main/java/com/example/adrian/monmatest/InicioActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class InicioActivity extends AppCompatActivity {
 
     private static int DURACION = 4000;
+    private static final String TAG = "activityInicio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        MyLog.d(TAG, "Iniciando onCreate...");//es como un print para mostrar mensajes y depurar
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
@@ -25,5 +30,50 @@ public class InicioActivity extends AppCompatActivity {
             }
         },DURACION);
 
+        MyLog.d(TAG, "Finalizando onCreate...");
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        MyLog.d(TAG, "Iniciando onStart...");//es como un print para mostrar mensajes y depurar
+        super.onStart();
+        MyLog.d(TAG, "Finalizando onStart...");
+    }
+
+    @Override
+    protected void onStop() {
+        MyLog.d(TAG, "Iniciando onStop...");//es como un print para mostrar mensajes y depurar
+        super.onStop();
+        MyLog.d(TAG, "Finalizando onStop...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        MyLog.d(TAG, "Iniciando onDestroy...");//es como un print para mostrar mensajes y depurar
+        super.onDestroy();
+        MyLog.d(TAG, "Finalizando onDestroy...");
+    }
+
+    @Override
+    protected void onPause() {
+        MyLog.d(TAG, "Iniciando onPause...");//es como un print para mostrar mensajes y depurar
+        super.onPause();
+        MyLog.d(TAG, "Finalizando onPause...");
+    }
+
+    @Override
+    protected void onResume() {
+        MyLog.d(TAG, "Iniciando onResume...");//es como un print para mostrar mensajes y depurar
+        super.onResume();
+        MyLog.d(TAG, "Finalizando onResume...");
+    }
+
+    @Override
+    protected void onRestart() {
+        MyLog.d(TAG, "Iniciando onRestart...");//es como un print para mostrar mensajes y depurar
+        super.onRestart();
+        MyLog.d(TAG, "Finalizando onRestart...");
     }
 }
