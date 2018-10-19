6d3b19d587a8a6317153e3efa8306b5d59dd6663
Adrian Carvajal <adrycarpe94@gmail.com>
2018-10-19 10:32:48 +0200

    Quinto commit (añadir menu)

--- app/src/main/java/com/example/adrian/monmatest/ResumenActivity.java
+++ app/src/main/java/com/example/adrian/monmatest/ResumenActivity.java
@@ -1,7 +1,11 @@
 package com.example.adrian.monmatest;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ResumenActivity extends AppCompatActivity {
 
@@ -11,6 +15,33 @@ public class ResumenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumen);
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menuActivity; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_nuevo:
+                Log.i("ActionBar", "Nuevo!");
+                return true;
+            case R.id.action_buscar:
+                Log.i("ActionBar", "Buscar!");;
+                return true;
+            case R.id.action_settings:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
