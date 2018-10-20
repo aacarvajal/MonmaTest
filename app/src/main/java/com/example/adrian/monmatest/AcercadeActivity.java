5f3fe5a545dd63571f79bede5ab0088dfff07b5c
Adrian Carvajal <adrycarpe94@gmail.com>
2018-10-20 17:15:13 +0200

    Sexto commit (Creacion de test en la pagina Resumen Activity, boton Up y Back y funcionalidad del test)

---
+++ app/src/main/java/com/example/adrian/monmatest/AcercadeActivity.java
@@ -0,0 +1,38 @@
+package com.example.adrian.monmatest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class AcercadeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acercade);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Asignar la acción necesaria. En este caso "volver atrás"
+                    onBackPressed();
+                }
+            });
+        } else {
+            Log.d("Acerca de", "Error al cargar toolbar");
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/adrian/monmatest/ResumenActivity.java
+++ app/src/main/java/com/example/adrian/monmatest/ResumenActivity.java
@@ -1,15 +1,25 @@
 package com.example.adrian.monmatest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class ResumenActivity extends AppCompatActivity {
 
     private static final String TAG = "ResumenInicio";
+
+    RadioButton rb1,rb2,rb3,rb4;
+    TextView texto;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,57 @@ public class ResumenActivity extends AppCompatActivity {
 
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
+
+        rb1 = (RadioButton)findViewById(R.id.radioResp1);
+        rb2 = (RadioButton)findViewById(R.id.radioResp2);
+        rb3 = (RadioButton)findViewById(R.id.radioResp3);
+        rb4 = (RadioButton)findViewById(R.id.radioResp4);
+        btn = (Button)findViewById(R.id.comprobar);
+        texto = (TextView)findViewById(R.id.respuesta);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                if(rb1.isChecked()){
+
+                    texto.setText("Incorrecto");
+
+                }else if(rb2.isChecked()){
+
+                    texto.setText("Incorrecto");
+
+                }else if(rb3.isChecked()){
+
+                    texto.setText("Incorrecto");
+
+                }else if(rb4.isChecked()){
+
+                    texto.setText("Correcto");
+
+                }
+
+            }
+
+        });
+
+
+    }
+
+    /*public void respuesta(){
+
+        TextView resp1 = (TextView)findViewById(R.id.resp1);
+        TextView resp2 = (TextView)findViewById(R.id.resp2);
+        TextView resp3 = (TextView)findViewById(R.id.resp3);
+        TextView resp4 = (TextView)findViewById(R.id.resp4);
+
+    }*/
+
+    @Override
+    public void onBackPressed() {//para volver hacia atras pulsando el boton retroceso
+        // Asignar la acción necesaria. En este caso terminar la actividad
+        finish();
     }
 
     @Override
@@ -34,10 +95,12 @@ public class ResumenActivity extends AppCompatActivity {
                 Log.i("ActionBar", "Nuevo!");
                 return true;
             case R.id.action_buscar:
-                Log.i("ActionBar", "Buscar!");;
+                Log.i("ActionBar", "Buscar!");
                 return true;
             case R.id.action_settings:
-                Log.i("ActionBar", "Settings!");;
+                Log.i("ActionBar", "Settings!");
+                Intent intent = new Intent(ResumenActivity.this,AcercadeActivity.class);
+                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
